2a319a14ae3df92a9534b7d2101526933abdab8d
SteveErle <therevolution777@yahoo.com>
2015-09-18 20:36:50 -0400

    Added findFiltIntensity in mainturtFreq

--- mainTurtleFreq.m
+++ mainTurtleFreq.m
@@ -35,8 +35,6 @@ signal = SigObj.getSignal();
 signalFilt = getFiltered(signal, filt, 'high');
 signalFilt = getFiltered(signalFilt, 0.123, 'low')+15;
 
-
-
 plot(signalFilt)
 hold on;
 plot(signal)
@@ -48,88 +46,30 @@ sigUnFiltPred = signal(day : day  + modLen+predLen);
 
 pred = Turtle(sigMod, sigPred, modLen, A, P);
 
-% evalGD = pred.predictTurtle('GD');
 evalBF = pred.predictTurtle('BF');
-
 evalBF.Total
 
-mm = evalBF.model_predict(1:modLen);
-mp = evalBF.model_predict(modLen:end);
-sm = sigPred(1:modLen);
-sp = sigPred(modLen:end);
+
+FindFiltIntensity = Evaluator(1,1,evalBF.model_predict);
+
+[tag1] = FindFiltIntensity.peakAndTrough(FindFiltIntensity.model_predict);
+target_num_of_extrema = length(tag1)
+
+signal = SigObj.getSignal();
+signalHighPass = getFiltered(signal, filt, 'high');
+sigHigh = signalHighPass(day : day  + modLen+predLen)+15;
 
 
+for filt_intensity = 0.075: 0.001:0.175
+    sigHighLow = getFiltered(sigHigh, filt_intensity, 'low');
+    [tag1, max1, min1] = FindFiltIntensity.peakAndTrough(sigHighLow);
+    if abs(length(tag1) - target_num_of_extrema) <= 1
+        found_filt_intensity = filt_intensity;
+        break
+    end
+end
 
-% EADstock = evalBF.sigPred;
-% EADmodel = evalBF.model_predict;
-% 
-% [tag1, max1, min1] = evalBF.peakAndTrough(EADstock);
-% [tag2, max2, min2] = evalBF.peakAndTrough(EADmodel);
-% 
-% % arrery = [];
-% % 
-% % for i = 1:length(max1)
-% % apple = abs(max1(i)-max2)
-% % 
-% % s = abs(i-find(min(apple) == apple))
-% % 
-% % arrery = [arrery; min(s)]
-% % end
-% % 
-% % sum(arrery)
-% 
-% 
-% 
-% % countSignal = SigObj.getSignal();
-% % countSignalFilt = getFiltered(signal, filt, 'high');
-% % countSigFilt = countSignalFilt(day : day  + modLen+predLen)+15;
-% % 
-% % 
-% % for i = 0.10: 0.001:0.15
-% % EADstock = getFiltered(countSigFilt, i, 'low');
-% % [tag1, max1, min1] = evalBF.peakAndTrough(EADstock);
-% % if abs(length(tag1) - 75) <= 1
-% %     i
-% %     break
-% % end 
-% %     
-% % 
-% % end
-% 
-% 
-% 
-% 
-% 
-% lse1 = sum((mm-sm).^2)
-% 
-% lse2 = sum((mp-sp).^2)
-% 
-% dmm = diff(mm);
-% dmp = diff(mp);
-% dsm = diff(sm);
-% dsp = diff(sp); 
-
-hold on;
-plot(sigUnFiltPred-8,'c')
-% % figure()
-% % plot(dmm,'k')
-% % hold on;
-% % plot(dmp,'b')
-% % hold on;
-% % plot(dsm, 'r');
-% % hold on;
-% % plot(dsp, 'r');
-% 
-% lse1 = sum((dmm-dsm).^2)
-% 
-% lse2 = sum((dmp-dsp).^2)
-% 
-% evalBF.Total
-
-
-
-
-
+found_filt_intensity
 
 % HeatMapofTurtles(signalFilt);
 % [totalX] = twoDMapOfTurtles(signal);
