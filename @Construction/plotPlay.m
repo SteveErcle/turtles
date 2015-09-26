981b5c8c95576f3806863e36acee1022b69f5a02
SteveErle <therevolution777@yahoo.com>
2015-09-26 19:08:42 -0400

    Added plotPlay

---
+++ @Construction/plotPlay.m
@@ -0,0 +1,49 @@
+
+function plotPlay(obj, projection, sigPlt, ticker)
+
+%% Plots the model and the prediction against the stock;
+
+
+sigMod  = obj.sigMod;
+
+% obj.plotPro(projection, sigPlt(1:length(sigMod)));
+%
+% pause;
+%
+% obj.plotPro(projection, sigPlt);
+
+
+
+predLen = obj.predLen;
+
+t = 1:length(projection);
+
+model = projection(1:end-predLen);
+prediction = projection(end-predLen:end);
+
+
+figure()
+for i = 1:2
+    
+    if i == 1
+        sigPlay = sigPlt(1:length(sigMod));
+    else
+        sigPlay = sigPlt(1:length(sigMod)+ticker);
+    end 
+        
+        tsg = 1:length(sigPlay);
+        plot(tsg, sigPlay, 'r')
+        
+        hold on;
+        plot(t(1:end-predLen), model, 'k')
+        hold on;
+        plot(t(end-predLen:end), prediction, 'b')
+        
+        axis([ 500 length(sigPlt) min(sigPlay(end-300:end)) max(sigPlay(end-300:end)) ]);
+        
+        pause;
+        
+end
+
+
+end
--- funcExperiment.m
+++ funcExperiment.m
@@ -2,46 +2,43 @@
 % getConstructInter()
 % getSteadyState()
 
-% 
+
 % clear all; close all; clc;
-% 
+%
 % P = [80 120];
-% theta = [pi/3 pi/2];
-% t = 1:300;
+% theta = [pi/4 pi/2];
+% t = 1:270;
 % model = cos(2*pi*1/P(1)*t + theta(1)) + 2*cos(2*pi*1/P(2)*t + theta(2)) ;
-% 
-% 
+%
+%
 % plot(model)
-% 
-% 
-% 
+%
+%
+%
 % conInt = [];
 % for ci = 1:length(P)
-%     
+%
 %     cDays = length(model) + theta(ci)/(2*pi)*P(ci);
 %     cDays =  mod(cDays, P(ci))
 %     if cDays > P(ci)/2
 %         cDays = P(ci) - cDays
 %     end
-%     
+%
 %     cPer  = 2*abs( 0.5 - (cDays/ P(ci)) );
 %     conInt = [conInt; cDays, cPer];
 % end
-% 
-% CItotal = sum(conInt(:,1))
-% 
-% 
-% 
-% pause
+%
+% CItotal = sum(conInt(:,1))/ (sum(P)/2)
+
 
 clear all
 clc
 close all
 
 stock = 'ABT';
-presentH = 1900;
+presentH = 1656;
 sigLen = 1000;
-predLen = 50;
+predLen = 20;
 
 A = [0.09 0.09 0.2 0.15 0.20 0.35 0.43 0.67];
 P = [18 25 34 43 62 99 142 178];
@@ -50,21 +47,20 @@ totals = [];
 sosals = [];
 founder = [];
 
-% for present = presentH %: presentH+100;
+
+lps = [1956;1776;2036;1796;2116;1656]
+
+hps = [2090;1390;1550;1680;1360;1380;1370]
+
+% parfor i = 0:1:25
 %
+% present = presentH+i*predLen;
 
-lps = [1450;1300;1600]
-
-hps = [1400;1700;1550]
-
-parfor i = 0:1:10
-
-present = presentH+i*predLen;
-
-% for i = 1:length(lps)
-%     
-%     present = lps(i)
-   
+for i = 1:length(lps)
+    
+    present = lps(i)
+    
+    % for present = presentH:3:presentH+100;
     
     sMod = SignalGenerator(stock, present, sigLen);
     [sig, sigHL] = sMod.getSignal('ac');
@@ -74,7 +70,6 @@ present = presentH+i*predLen;
     t.type = 1;
     [theta] = t.getTheta('BF');
     
-    
     c = Construction(A, P, theta, predLen, sigMod);
     [model, prediction, projection] = c.constructPro();
     
@@ -82,43 +77,36 @@ present = presentH+i*predLen;
     for ci = 1:length(P)
         
         cDays = length(model) + theta(ci)/(2*pi)*P(ci);
-        cDays =  mod(cDays, P(ci))
+        cDays =  mod(cDays, P(ci));
         if cDays > P(ci)/2
-            cDays = P(ci) - cDays
+            cDays = P(ci) - cDays;
         end
         
         %     cPer  = 2*abs( 0.5 - (cDays/ P(ci)) );
         conInt = [conInt; cDays];
     end
-    
-    CItotal = sum(conInt(:,1));
-    
-    diffMod = diff(model);
+    CItotal = sum(conInt(:,1)) / (sum(P)/2);
     
     sPro = SignalGenerator(stock, present+predLen, sigLen+predLen);
     [sig, sigHL] = sPro.getSignal('ac');
     sigPro = sigHL + mean(sig(end-100:end));
     
-    c.plotPro(projection, sigPro);
-    c.plotPro(projection, sig);
+        ticker = 3;
+        c.plotPlay(projection, sig, ticker);
+    
+    %     c.plotPro(projection, sig);
     
     e = Evaluator(sigMod, model, prediction);
     
-    nextInflection = e.peakAndTrough(prediction);
-    nextInflection = nextInflection(2);
-    
-    percDiff = abs((prediction(nextInflection) - model(end-1))/model(end-1))
-    nextInflection
-    
-    total0 = e.percentReturn(prediction)
-    total1 = e.percentReturn(sigPro)
-    total2 = e.percentReturn(sig)
+    total0 = e.percentReturn(prediction);
+    total1 = e.percentReturn(sigPro);
+    total2 = e.percentReturn(sig);
     [modDVE modDVEList] = e.DVE();
     
     loadErUp = [present, CItotal, total0, total1, total2];
-    
     founder = [founder; loadErUp];
     
+        close all
     
 end
 
