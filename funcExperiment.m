eb2d0df361cc91c9e5a209395d1478230bb241d3
SteveErle <therevolution777@yahoo.com>
2015-09-26 17:49:42 -0400

    Added constructInter finder

--- funcExperiment.m
+++ funcExperiment.m
@@ -1,141 +1,126 @@
-% Stop loss, stop limit & open to close
-% "" "" optimization
+
+% getConstructInter()
+% getSteadyState()
+
+% 
+% clear all; close all; clc;
+% 
+% P = [80 120];
+% theta = [pi/3 pi/2];
+% t = 1:300;
+% model = cos(2*pi*1/P(1)*t + theta(1)) + 2*cos(2*pi*1/P(2)*t + theta(2)) ;
+% 
+% 
+% plot(model)
+% 
+% 
+% 
+% conInt = [];
+% for ci = 1:length(P)
+%     
+%     cDays = length(model) + theta(ci)/(2*pi)*P(ci);
+%     cDays =  mod(cDays, P(ci))
+%     if cDays > P(ci)/2
+%         cDays = P(ci) - cDays
+%     end
+%     
+%     cPer  = 2*abs( 0.5 - (cDays/ P(ci)) );
+%     conInt = [conInt; cDays, cPer];
+% end
+% 
+% CItotal = sum(conInt(:,1))
+% 
+% 
+% 
+% pause
 
 clear all
 clc
 close all
 
 stock = 'ABT';
-presentH = 1852;
+presentH = 1900;
 sigLen = 1000;
-predLen = 20;
+predLen = 50;
 
 A = [0.09 0.09 0.2 0.15 0.20 0.35 0.43 0.67];
 P = [18 25 34 43 62 99 142 178];
-
-% construct Interference finder
 
 totals = [];
 sosals = [];
 founder = [];
 
-show = [];
+% for present = presentH %: presentH+100;
+%
 
-for present = presentH %: presentH+100;
-    
-    present
-    
-    % for i = 0:1:10
-    
-    %     present = presentH+i*predLen
+lps = [1450;1300;1600]
+
+hps = [1400;1700;1550]
+
+parfor i = 0:1:10
+
+present = presentH+i*predLen;
+
+% for i = 1:length(lps)
+%     
+%     present = lps(i)
+   
     
     sMod = SignalGenerator(stock, present, sigLen);
     [sig, sigHL] = sMod.getSignal('ac');
-    sigMod = sigHL + mean(sig);
+    sigMod = sigHL + mean(sig(end-100:end));
     
-    t = TideFinder(sigMod, A, P)
-    t.type = 2;
+    t = TideFinder(sigMod, A, P);
+    t.type = 1;
     [theta] = t.getTheta('BF');
+    
     
     c = Construction(A, P, theta, predLen, sigMod);
     [model, prediction, projection] = c.constructPro();
     
-    diffMod = diff(model);
-    
-    if sign(diffMod(end-1)) ~= sign(diffMod(end))
+    conInt = [];
+    for ci = 1:length(P)
         
-           
-        sPro = SignalGenerator(stock, present+predLen, sigLen+predLen);
-        [sig, sigHL] = sPro.getSignal('ac');
-        sigPro = sigHL + mean(sig);
-        
-        c.plotPro(projection, sigPro);
-        c.plotPro(projection, sig);
-        
-        e = Evaluator(sigMod, model, prediction);
-        
-        nextInflection = e.peakAndTrough(prediction);
-        nextInflection = nextInflection(2);
-        
-        
-        
-        percDiff = abs((prediction(nextInflection) - model(end-1))/model(end-1))
-        nextInflection
-        
-        if percDiff > 0.05 & nextInflection > 7
-            
-            present
-            
-            total0 = e.percentReturn(prediction)
-            total1 = e.percentReturn(sigPro)
-            total2 = e.percentReturn(sig)
-            [mod modl] = e.DVE();
-            
-            founder = [founder; present];
-            
+        cDays = length(model) + theta(ci)/(2*pi)*P(ci);
+        cDays =  mod(cDays, P(ci))
+        if cDays > P(ci)/2
+            cDays = P(ci) - cDays
         end
         
+        %     cPer  = 2*abs( 0.5 - (cDays/ P(ci)) );
+        conInt = [conInt; cDays];
     end
     
+    CItotal = sum(conInt(:,1));
+    
+    diffMod = diff(model);
+    
+    sPro = SignalGenerator(stock, present+predLen, sigLen+predLen);
+    [sig, sigHL] = sPro.getSignal('ac');
+    sigPro = sigHL + mean(sig(end-100:end));
+    
+    c.plotPro(projection, sigPro);
+    c.plotPro(projection, sig);
+    
+    e = Evaluator(sigMod, model, prediction);
+    
+    nextInflection = e.peakAndTrough(prediction);
+    nextInflection = nextInflection(2);
+    
+    percDiff = abs((prediction(nextInflection) - model(end-1))/model(end-1))
+    nextInflection
+    
+    total0 = e.percentReturn(prediction)
+    total1 = e.percentReturn(sigPro)
+    total2 = e.percentReturn(sig)
+    [modDVE modDVEList] = e.DVE();
+    
+    loadErUp = [present, CItotal, total0, total1, total2];
+    
+    founder = [founder; loadErUp];
     
     
 end
 
 
-
-
-%         slope = abs((prediction(end)-prediction(1)) / prediction(1));
-%
-%         tt = [total0, total1, total2, slope, sum(modl(end-200:end))];
-%
-%         if total0 > 1
-%             ss = [total0, total1, total2, slope, sum(modl(end-200:end))];
-%             sosals = [sosals ;ss];
-%         end
-%
-%         totals = [totals; tt];
-
-
-figure()
-plot((totals(:,2)))
-hold on;
-plot((totals(:,3)),'r')
-
-sum(totals(:,1))
-sum(totals(:,2))
-sum(totals(:,3))
-
-figure()
-plot((sosals(:,2)))
-hold on;
-plot((sosals(:,3)),'r')
-
-sum(sosals(:,1))
-sum(sosals(:,2))
-sum(sosals(:,3))
-
-% figure()
-% plot(totals(:,3),totals(:,1), '*')
-% hold on
-% plot(totals(:,3),totals(:,2), 'r*')
-% title('Sloper')
-%
-% figure()
-% plot(totals(:,4),totals(:,1), '*')
-% hold on
-% plot(totals(:,4),totals(:,2), 'r*')
-% title('diffErrorer')
-
-
-
-
-% open to close;
-% stop loss
-% stop limit
-
-
-
-
-
-
-
+sfd = sortrows(founder,2)
--- mainExperiment.m
+++ mainExperiment.m
@@ -1,96 +1,99 @@
-% main Experiment
+% Stop loss, stop limit & open to close
+% "" "" optimization
+% Watch by hand on HL with no candles
+% "" with candles
 
-
-%% This is for experimenting with mainTurtles
-
-clear all; close all; clc;
-
-
-%% Declare
+clear all
+clc
+close all
 
 stock = 'ABT';
+presentH = 1200;
+sigLen = 1000;
+predLen = 20;
 
-RANSTEP = 0;
-FAKER   = 0;
-REALER  = 1;
+A = [0.09 0.09 0.2 0.15 0.20 0.35 0.43 0.67];
+P = [18 25 34 43 62 99 142 178];
 
-filt = 0.005;
-present = 2400;
-signal_length = 2300;
-modLen = 1000;
-predLen = 1;
-day = 500;
-
-if FAKER == 1
-    A = [0.5, 1, 1.5, 2, 4, 7]/4;
-    P = [24, 31, 52, 84, 122, 543];
-elseif REALER == 1
-    A = [0.09 0.09 0.2 0.15 0.20 0.35 0.43 0.67];
-%     P = [18 25 31 43 62 99 142 178];
-    P = [18 25 34 43 62 99 142 178];
-   
-end
-ph = [1.5, 2.4, 0.4, 1.2 , .9, 1.2, 1.3, 4.5];
-
-%% Initialize
-SigObj = SignalGenerator(stock, present, signal_length, RANSTEP, FAKER, REALER, ph, A, P);
-
-%% Run
-
-ix = [];
-
-%% Testing recalculator
-
-
-ix = [];
+% getConstructInter()
+% getSteadyState()
 
 totals = [];
+sosals = [];
+founder = [];
 
+show = [];
 
-daz = 1100;
+% for present = presentH %: presentH+100;
+%
+%     present
 
-day = daz;
-
-signal = SigObj.getSignal();
-
-parfor day = daz : daz+25
-
-sigPredUnfilt = signal(day : day  + modLen+predLen);
-sigMod = signal(day : day  + modLen);
-sigPred = signal(day : day  + modLen+predLen);
-
-sigMod = getFiltered(sigMod, filt, 'high');
-% sigMod = getFiltered(sigMod, 0.123, 'low');
-sigPred = getFiltered(sigPred, filt, 'high');
-% sigPred = getFiltered(sigPred, 0.123, 'low');
-
-sigPredUnfilt = sigPredUnfilt;
-sigMod = sigMod + 15;
-sigPred = sigPred + 15;
-
-pred1 = Turtle(sigMod, sigPred, modLen, A, P);
-pred1.type = 2;
-pred1.sigPredUnfilt = sigPredUnfilt;
-
-evalBF1 = pred1.predictTurtle('BF');
-evalBF1.Total;
-evalBF1.DVE();
-std(evalBF1.sigPred)/mean(evalBF1.sigPred);
-
-slope = diff(evalBF1.model_predict);
-day
-slope = abs(slope(end))/mean(evalBF1.model_predict)
-
-if slope > 0.0033
-    totals = [totals; evalBF1.Total];
+for i = 0:1:5
+    
+    present = presentH+i*predLen
+    
+    
+    sMod = SignalGenerator(stock, present, sigLen);
+    [sig, sigHL] = sMod.getSignal('ac');
+    sigMod = sigHL + mean(sig(end-100:end));
+    
+    t = TideFinder(sigMod, A, P)
+    t.type = 2;
+    [theta] = t.getTheta('BF');
+    
+    c = Construction(A, P, theta, predLen, sigMod);
+    [model, prediction, projection] = c.constructPro();
+    
+    conInt = [];
+    for ci = P
+        conInt = [conInt; mod(length(model),ci)];
+    end
+    
+    diffMod = diff(model);
+    
+    if sign(diffMod(end-1)) ~= sign(diffMod(end))
+        
+        
+        sPro = SignalGenerator(stock, present+predLen, sigLen+predLen);
+        [sig, sigHL] = sPro.getSignal('ac');
+        sigPro = sigHL + mean(sig(end-100:end));
+        
+        c.plotPro(projection, sigPro);
+        c.plotPro(projection, sig);
+        
+        e = Evaluator(sigMod, model, prediction);
+        
+        nextInflection = e.peakAndTrough(prediction);
+        nextInflection = nextInflection(2);
+        
+        
+        
+        percDiff = abs((prediction(nextInflection) - model(end-1))/model(end-1))
+        nextInflection
+        
+        if percDiff > 0.04 & nextInflection > 7
+            
+            present
+            
+            total0 = e.percentReturn(prediction)
+            total1 = e.percentReturn(sigPro)
+            total2 = e.percentReturn(sig)
+            [mod modl] = e.DVE();
+            
+            founder = [founder; present];
+            
+        end
+        
+    end
+    
+    
+    
 end
 
-end 
 
 
-totals
 
-sum(totals)
+
 
 
 
