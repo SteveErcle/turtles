0c190e9e2f5299abc793517c0a042641b168174e
SteveErle <therevolution777@yahoo.com>
2015-11-03 19:16:11 -0500

    pass and select checkpoint A

--- @Evaluator/Evaluator.m
+++ @Evaluator/Evaluator.m
@@ -1,69 +1,90 @@
 classdef Evaluator
     
-   properties
-       
-       sigMod;
-       model;
-       prediction;
-       stop_loss = 10;
-       one_trade = 0;
-       Total;
-       Type;
-       modMME;
-       predMME;
-       
-       
-   end
-   
-   methods
-       
-       function obj = Evaluator(sigMod, model, prediction)
-           
-           obj.sigMod       = sigMod;
-           obj.model        = model;
-           obj.prediction   = prediction;
-           
-          
-       end
-       
-       function [modDVE, modDVElist] = DVE(obj)
-           
-           dmm = diff(obj.model');
-           
-           dsm = diff(obj.sigMod);
-          
-           modDVElist = (dmm-dsm).^2;
-           
-           modDVE = sum(modDVElist);
-           
-       end 
-       
-       
-      [Total] = percentReturn(obj, sigCmp);
-      
-      [modMME, predMME, modMMElist, predMMElist] = MME(obj);
-      
-      [tagged,s_imax,s_imin] = peakAndTrough(obj, graph);
-      
-      
-      
-      % mm = evalBF.model_predict(1:modLen);
-% mp = evalBF.model_predict(modLen:end);
-% sm = sigPred(1:modLen);
-% sp = sigPred(modLen:end);
-% 
-% lse1 = sum((mm-sm).^2)
-% 
-% lse2 = sum((mp-sp).^2)
-% 
-
-% 
-% 
-% 
-
-  
-       
-   end  
+    properties
+        
+        sigMod;
+        model;
+        prediction;
+        stop_loss = 10;
+        one_trade = 0;
+        Total;
+        Type;
+        modMME;
+        predMME;
+        
+        
+    end
     
-end
-
+    methods
+        
+        function obj = Evaluator(sigMod, model, prediction)
+            
+            obj.sigMod       = sigMod;
+            obj.model        = model;
+            obj.prediction   = prediction;
+            
+            
+        end
+        
+        function [modDVE, modDVElist] = DVE(obj)
+            
+            dmm = diff(obj.model');
+            
+            dsm = diff(obj.sigMod);
+            
+            modDVElist = (dmm-dsm).^2;
+            
+            modDVE = sum(modDVElist);
+            
+        end
+        
+        function [Total] = prOHLC(obj, sigProMtrx)
+            
+            sigPro = sigProMtrx;
+            prediction = obj.prediction;
+            spmh = max(sigPro(end-(predLen-1):end,2));
+            spml = min(sigPro(end-(predLen-1):end,3));
+            spec = sigPro(end-predLen,4);
+            
+            if prediction(end) > prediction(1)
+                (spmh - spec)/spec*100
+                (spml - spec)/spec*100
+            else
+                (spec - spmh)/spec*100
+                (spec - spml)/spec*100
+            end
+            
+            Total = 1;
+            
+        end 
+            
+            
+            
+            [Total] = percentReturn(obj, sigCmp);
+            
+            [modMME, predMME, modMMElist, predMMElist] = MME(obj);
+            
+            [tagged,s_imax,s_imin] = peakAndTrough(obj, graph);
+            
+            
+            
+            % mm = evalBF.model_predict(1:modLen);
+            % mp = evalBF.model_predict(modLen:end);
+            % sm = sigPred(1:modLen);
+            % sp = sigPred(modLen:end);
+            %
+            % lse1 = sum((mm-sm).^2)
+            %
+            % lse2 = sum((mp-sp).^2)
+            %
+            
+            %
+            %
+            %
+            
+            
+            
+        end
+        
+    end
+    
--- sinreTestMultiplePeriods.m
+++ sinreTestMultiplePeriods.m
@@ -12,28 +12,28 @@ addpath('/Users/Roccotepper/Documents/turtles/sinide')
 stock = 'JPM'
 
 
-day = 1000;
+day = 1050;
 futer = 1000;
 interval = 25
 
-predLen = 25;
+predLen = 1;
 sampLen = 50;
 dc_offset = 15;
 
 dataliousMajor = [];
 dataliousMinor = [];
+totalious = [];
 
 expectoProptronum = [];
 
 % theSamples = [25, 120, 300]
 
-
-parfor i = 0:futer/interval
+for i = 0:futer/interval
     present = day + i*interval
     
     expectoProptronum = [];
     
-    for sampLen = 500:100:500
+    for sampLen = 35:50:100
         
         filtL = 0.0550;
         filtH = 0.0065;
@@ -62,9 +62,9 @@ parfor i = 0:futer/interval
         c = Construction(A, P, theta, predLen, sigMod);
         [model, prediction, projection] = c.constructPro();
         %         c.plotPlay(projection, sigPro);
-        c.plotPro(projection, sigPro);
-        hold on;
-        plot(sigMod,'m')
+        %         c.plotPro(projection, sigPro);
+        %         hold on;
+        %         plot(sigMod,'m')
         
         
         
@@ -80,18 +80,25 @@ parfor i = 0:futer/interval
         [thetaShift] = shiftTheta(P, sampLens, theta);
         
         sMod = SignalGenerator(stock, present+2, min(sampLens));
-        [sigMod, sigHL, sigH, sigL] = sMod.getSignal('ac', filtH, filtL);
+        [sigMod, sigHL, sigH, sigL] = sMod.getSignal('c', filtH, filtL);
         sPro = SignalGenerator(stock, present+2+predLen, min(sampLens)+predLen);
-        [sigPro, sigHL, sigH, sigL] = sPro.getSignal('ac', filtH, filtL);
+        [sigPro, sigHL, sigH, sigL] = sPro.getSignal('c', filtH, filtL);
         
-        %         for i = 1:length(sampLens)
-        %             c = Construction(A(i), P(i), thetaShift(i), predLen, sigMod);
-        %             [model, prediction, projection] = c.constructPro();
-        %             %     c.plotPlay(projection, sigPro);
-        %             c.plotPro(projection-mean(projection) + mean(sigPro), sigPro);
-        %             hold on;
-        %             plot(sigMod,'m')
-        %         end
+        
+        sMtx = SignalGenerator(stock, present+2+predLen, min(sampLens)+predLen);
+        [sigMtx, sigHL, sigH, sigL] = sMtx.getSignal('all', filtH, filtL);
+        
+        
+        
+        
+%                 for i = 1:length(sampLens)
+%                     c = Construction(A(i), P(i), thetaShift(i), predLen, sigMod);
+%                     [model, prediction, projection] = c.constructPro();
+%                     %     c.plotPlay(projection, sigPro);
+%                     c.plotPro(projection-mean(projection) + mean(sigPro), sigPro);
+%                     hold on;
+%                     plot(sigMod,'m')
+%                 end
         %
         %         theta
         %         thetaShift
@@ -100,13 +107,39 @@ parfor i = 0:futer/interval
         
         c = Construction(A, P, thetaShift, predLen, sigMod);
         [model, prediction, projection] = c.constructPro();
-        %     c.plotPlay(projection, sigPro);
+        %         c.plotPlay(projection-mean(projection) + mean(sigPro), sigPro);
         
-        pause;
+        %         pause;
+        
+        
+        sph = sigMtx(end-(predLen-1):end,2);
+        spl = sigMtx(end-(predLen-1):end,3);
+        spt = length(sigPro)-(predLen-1):length(sigPro);
+        
+        spmh = max(sigMtx(end-(predLen-1):end,2));
+        spml = min(sigMtx(end-(predLen-1):end,3));
+        
+        speo = sigMtx(end-(predLen-1),1);
+        spec = sigMtx(end,4);
+        
+        if prediction(end) > prediction(1)
+            111
+            totalious = [totalious; (spmh - speo)/speo*100 , (spml - speo)/speo*100, (spec - speo)/speo*100, i]
+        else
+            222
+            totalious = [totalious; (speo - spml)/speo*100 ,(speo - spmh)/speo*100, (speo - spec)/speo*100, i]
+        end
+        
+        
         c.plotPro(projection-mean(projection) + mean(sigPro), sigPro);
         hold on;
         plot(sigMod,'m')
+        plot(spt,sph,'ks')
+        plot(spt,spl,'ko')
+        plot(spt(1),speo,'b*');
+        hold off
         
+        drawnow
         
         e = Evaluator(sigMod, model, prediction);
         pr = e.percentReturn(sigPro)
@@ -116,13 +149,13 @@ parfor i = 0:futer/interval
         pSumo = sum(posList);
         
         
-        dataliousMinor = [i, present, pr, P];
+        dataliousMinor = [i, present, pr];
         
         dataliousMajor = [dataliousMajor; dataliousMinor];
         
-        pause
+%         pause
         
-        close all
+%         close all
         
         
     end
@@ -130,5 +163,38 @@ parfor i = 0:futer/interval
 end
 
 sprintf('Done')
+
+stopLoss = -1;
+stopLimit = 5;
+
+TotalSum = [];
+
+% for stopLimit = 3:0.25:10
+%     for stopLoss = -0.25:-0.25:-3
+%         
+        totaliousMajor = [];
+        
+        for i = 1:length(totalious)
+            if totalious(i,2) <= stopLoss;
+                totaliousMajor = [totaliousMajor; stopLoss];
+                
+            elseif totalious(i,1) >= stopLimit;
+                totaliousMajor = [totaliousMajor; stopLimit];
+            else
+                totaliousMajor = [totaliousMajor; totalious(i,3)];
+            end
+            
+        end
+        
+        TotalSum = [TotalSum; sum(totaliousMajor), stopLimit, stopLoss];
+        
+%     end
+% end
+
+
+TotalSum = sortrows(TotalSum,-1);
+
+totalious = sortrows(totalious,-1);
+
 
 
