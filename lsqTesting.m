23476af681cd7ff0b636aa8e71f816dd85f3be3b
FifthShogun <el.awadallah@gmail.com>
2015-11-06 21:10:09 -0500

    lsqTesting removed amplitudes from search, not finding periods

--- lsqTesting.m
+++ lsqTesting.m
@@ -32,17 +32,16 @@ end
 % y = y + rander;
 
 
-fun = @(x)loopClosure(theta, t, y, x);
+fun = @(x)loopClosure(A, t, y, x);
 
 
 resMajor = [];
 
 parfor i = 1:50
     
-guess = zeros(length(theta),3);
-guess(:,1) = rand(length(theta),1)*5;
-guess(:,2) = rand(length(theta),1)*150 %[150, 75, 50, 25]+rand(1,length(theta))*10;
-guess(:,3) = rand(length(theta),1)*2*pi;
+guess = zeros(length(theta),2);
+guess(:,1) = rand(length(theta),1)*150 %[150, 75, 50, 25]+rand(1,length(theta))*10;
+guess(:,2) = rand(length(theta),1)*2*pi;
 guess = guess';
 guess = guess(:);
 guess(end+1) = mean(y);
@@ -60,12 +59,12 @@ resMajor = sortrows(resMajor,1);
 x = resMajor(1,2:end)
 
 model = 0;
-for i = 1:3:length(theta)*3
+for i = 1:2:length(theta)*2
     
-   model = model + x(i)*cos(2*pi*1/x(i+1)*t + x(i+2));
+   model = model + A((i+1)/2)*cos(2*pi*1/x(i)*t + x(i+1));
    
 end
-model = x(length(theta)*3 + 1) + model;
+model = x(length(theta)*2 + 1) + model;
 
 plot(y,'r')
 hold on
