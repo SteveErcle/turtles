b55c50f2b8cca4ef2e7ab12e488ebb129ec88edd
SteveErle <therevolution777@yahoo.com>
2015-11-18 22:30:22 -0500

    moonSim update

--- eggLayerMoonSim.m
+++ eggLayerMoonSim.m
@@ -6,14 +6,14 @@ clc;
 
 
 c = yahoo;
-stock = 'COST';
+stock = 'MENT';
 
 
 yearlyHL = [];
 
 
-year1 = 1998;
-year2 = 2000 + 1;
+year1 = 2006;
+year2 = 2000 + 11;
 month = 1;
 
 monthDay = strcat(num2str(month),'/33/');
@@ -22,7 +22,7 @@ monthDay = strcat(num2str(month),'/33/');
 w = fetch(c,stock,strcat('1/1/', num2str(year1)),...
     strcat(monthDay, num2str(year2)), 'w');
 
-for i = 50:200
+for i = 100:400
     highlow(w(end-i:end,3), w(end-i:end,4),...
         w(end-i:end,3), w(end-i:end,4),'blue',w(end-i:end,1));
     
