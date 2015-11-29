694c27a9c8fff089fef965e76c2fc17397af66af
SteveErle <therevolution777@yahoo.com>
2015-11-29 14:36:39 -0500

    added formating

--- getGuiDat.m
+++ getGuiDat.m
@@ -5,43 +5,86 @@ close all
 
 
 
-stock = 'MENT';
+stock = 'DBC';
 c = yahoo;
-w = fetch(c,stock,now, now-2000, 'w');
+w = fetch(c,stock,now, now-6000, 'm');
 close(c)
 
 handles = guihandles(giua);
 
 
-highlowFlex(handles.axesG, w(:,3), w(:,4),...
+% highlowFlex(handles.axesG, w(:,3), w(:,4),...
+%     w(:,3), w(:,4),'blue',w(:,1));
+
+
+h = highlow(w(:,3), w(:,4),...
     w(:,3), w(:,4),'blue',w(:,1));
+set(gcf, 'Position', [0, 0, 1460, 700]);
+
+set(giua, 'Position', [15, 210, 210, 5]);
+
+hold on;
 
 
-
-initView = 150;
+initView = 80
 set(handles.slider1, 'Value', 0);
 set(handles.slider1, 'Max', size(w,1)-initView-1, 'Min', 0);
-set(handles.slider1, 'SliderStep', [1/size(w,1), 10/size(w,1)]);
+set(handles.slider1, 'SliderStep', [1/(size(w,1)-initView), 10/(size(w,1)-initView)]);
 
+highestR = 46.63;
+lowestS  = 17.94;
+for i = 1:9
+    fluctLevs(i) = (highestR-lowestS)*((i-1)/8)+lowestS;
+end
+fluctLevs(end+1) = (highestR-lowestS)*(1/3)+lowestS
+fluctLevs(end+1) = (highestR-lowestS)*(2/3)+lowestS
 
-levels = [0 12.5 25 33 37.5 50 62.5 67 75 87.5 100 150 250]';
-bottomLevs = 8.21*(1+levels/100);
-
-for i = 1:length(bottomLevs)
-    set(handles.axesG, 'NextPlot', 'add');
+for i = 1:length(fluctLevs)
+    %     set(handles.axesG, 'NextPlot', 'add');
     tLevs = w(end,1):w(1,1);
-    plot(handles.axesG, tLevs , ones(1,length(tLevs))*bottomLevs(i), 'k')
+    %     plot(handles.axesG, tLevs , ones(1,length(tLevs))*fluctLevs(i), 'k')
+    if i <= length(fluctLevs) - 2
+        plot(tLevs , ones(1,length(tLevs))*fluctLevs(i), 'k')
+    else
+        plot(tLevs , ones(1,length(tLevs))*fluctLevs(i), 'c')
+    end
+    
+end
+
+for i = 1:9
+    highLevs(i) = highestR*((i-1)/8);
+end
+highLevs(end+1) = highestR*1/3;
+highLevs(end+1) = highestR*2/3;
+
+
+for i = 1:length(highLevs)
+    %     set(handles.axesG, 'NextPlot', 'add');
+    tLevs = w(end,1):w(1,1);
+    %     plot(handles.axesG, tLevs , ones(1,length(tLevs))*highLevs(i), 'g')
+    if i <= length(highLevs) - 2
+        plot(tLevs , ones(1,length(tLevs))*highLevs(i), 'r')
+    else
+        plot(tLevs , ones(1,length(tLevs))*highLevs(i), 'm')
+    end
 end
 
 
 while(true)
-   
-    val = get(handles.slider1,'Value')
+    
+    val = get(handles.slider1,'Value');
+    
     startIndx = ceil(val);
     endIndx  = ceil(val)+initView;
-    set(handles.axesG, 'XLim', [w(end - startIndx,1)+4, w(end - endIndx,1)+4]);
-    set(handles.axesG, 'YLim', [min(w(end-endIndx:end-startIndx,4))*0.9, max(w(end-endIndx:end-startIndx,3))*1.1]);
-    datetick(handles.axesG,'x',12, 'keeplimits');
     
-    pause(0.025) 
+    %     set(h, 'XLim', [w(end - startIndx,1)+4, w(end - endIndx,1)+4]);
+    %     set(h, 'YLim', [min(w(end-endIndx:end-startIndx,4))*0.99, max(w(end-endIndx:end-startIndx,3))*1.01]);
+    %     datetick(h, 'x',12, 'keeplimits');
+    
+    axis([w(end - startIndx,1)+4, w(end - endIndx,1)+4,...
+        min(w(end-endIndx:end-startIndx,4))*0.99, max(w(end-endIndx:end-startIndx,3))*1.01]);
+    
+    datetick('x',12, 'keeplimits');
+    
+    pause(0.025)
 end
--- giua.m
+++ giua.m
@@ -22,7 +22,7 @@ function varargout = giua(varargin)
 
 % Edit the above text to modify the response to help giua
 
-% Last Modified by GUIDE v2.5 25-Nov-2015 14:03:54
+% Last Modified by GUIDE v2.5 29-Nov-2015 13:48:09
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -117,6 +117,28 @@ function slider4_Callback(hObject, eventdata, handles)
 % --- Executes during object creation, after setting all properties.
 function slider4_CreateFcn(hObject, eventdata, handles)
 % hObject    handle to slider4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+% --- Executes on slider movement.
+function slider5_Callback(hObject, eventdata, handles)
+% hObject    handle to slider5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function slider5_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider5 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
