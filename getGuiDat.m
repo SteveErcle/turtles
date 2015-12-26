2742ace8432333e0adc9922cf3b823641d7e92a3
SteveErle <therevolution777@yahoo.com>
2015-12-26 13:35:25 -0500

    Added daily

--- getGuiDat.m
+++ getGuiDat.m
@@ -8,28 +8,39 @@ stock = 'USO'; %XLE
 c = yahoo;
 m = fetch(c,stock,now, now-7000, 'm');
 w = fetch(c,stock,now, now-7000, 'w');
+d = fetch(c,stock,now, now-7000, 'd');
 close(c)
+
 
 handles = guihandles(giua);
 
-subplot(2,1,1)
-h1 = highlow(m(:,3), m(:,4),...
+subplot(3,1,1)
+highlow(m(:,3), m(:,4),...
     m(:,3), m(:,4),'blue',m(:,1));
+title('Monthly')
 
-subplot(2,1,2)
-h2 = highlow(w(:,3), w(:,4),...
-    w(:,3), w(:,4),'blue',w(:,1));
-hold on
 
-hi = w(:,3); lo = w(:,4); cl = w(:,5); op = w(:,2); da = w(:,1);
-subplot(2,1,2)
+subplot(3,1,2)
+hi = d(:,3); lo = d(:,4); cl = d(:,3); op = d(:,4); da = d(:,1);
 highlow(hi, lo, cl, op, 'blue', da);
+title('Daily')
+
+subplot(3,1,3)
+highlow(w(:,3), w(:,4),...
+    w(:,3), w(:,4),'blue',w(:,1));
+title('Weekly')
 hold on
+
+subplot(3,1,3)
+hi = w(:,3); lo = w(:,4); cl = w(:,5); op = w(:,2); da = w(:,1);
+highlow(hi, lo, cl, op, 'blue', da);
+
 
 lineHiLo  = lineSetter(gcf, 1)
 set(lineHiLo,'Visible','off')
 
-set(gcf, 'Position', [0, 0, 1460, 700]);
+% set(gcf, 'Position', [0, 0, 1460, 700]);
+set(gcf, 'Position', [-1077,-1017,1077,1822]);
 set(giua, 'Position', [15, 210, 210, 5]);
 
 initView = 100;
@@ -41,22 +52,24 @@ set(handles.view, 'Value', 0);
 
 tLevs = m(end,1):m(1,1);
 
-subplot(2,1,2)
+subplot(3,1,3)
 highest = 0;
 lowest  = 0;
 showFluctLevs(highest, lowest, tLevs)
-linFluct = lineSetter(gcf, 14)
+linFluct = lineSetter(gcf, 14);
 set(linFluct,'Visible','off')
 
-subplot(2,1,2)
+subplot(3,1,3)
 showHighLevs(highest, tLevs)
-linHi = lineSetter(gcf, 14)
+linHi = lineSetter(gcf, 14);
 set(linHi,'Visible','off')
 
 
 
 values = [];
 flagView = -1;
+flagHi = -1;
+flagFluct = -1;
 while(true)
     
     val = get(handles.slider1,'Value');
@@ -70,11 +83,15 @@ while(true)
                 value = getfield(cursor_info, {i},'Position');
                 datestr(value)
                 values = [values;value(2)];
-                subplot(2,1,1)
+                subplot(3,1,1)
                 hold on
                 plot(tLevs , ones(1,length(tLevs))*value(2), 'k')
                 
-                subplot(2,1,2)
+                subplot(3,1,2)
+                hold on
+                plot(tLevs , ones(1,length(tLevs))*value(2), 'k')
+                
+                subplot(3,1,3)
                 hold on
                 plot(tLevs , ones(1,length(tLevs))*value(2), 'k')
                 
@@ -97,36 +114,34 @@ while(true)
         end
         
         if get(handles.viewHiLevs, 'Value')
-            if flagView == -1
+            if flagHi == -1
                 set(linHi,'Visible','on')
-                flagView = flagView*-1;
+                flagHi = flagHi*-1;
             else
                 set(linHi,'Visible','off')
-                flagView = flagView*-1;
+                flagHi = flagHi*-1;
             end
             set(handles.viewHiLevs, 'Value', 0);
         end
         
         if get(handles.viewFluctLevs, 'Value')
-            if flagView == -1
+            if flagFluct == -1
                 set(linFluct,'Visible','on')
-                flagView = flagView*-1;
+                flagFluct = flagFluct*-1;
             else
                 set(linFluct,'Visible','off')
-                flagView = flagView*-1;
+                flagFluct = flagFluct*-1;
             end
             set(handles.viewFluctLevs, 'Value', 0);
         end
     end
     
-    
-    
     if get(handles.setHiLo, 'Value')
         
-%         dataTips = findall(gca, 'Type', 'hggroup', 'HandleVisibility', 'off');
-%         if length(dataTips) > 0
-%             delete(dataTips);
-%         end
+        dataTips = findall(gca, 'Type', 'hggroup', 'HandleVisibility', 'off');
+        if length(dataTips) > 0
+            delete(dataTips);
+        end
         
         set(linFluct,'Visible','off')
         set(linHi,'Visible','off')
@@ -134,12 +149,12 @@ while(true)
         highest = str2double(get(handles.HiLevView,'String'));
         lowest = str2double(get(handles.LoLevView,'String'));
         
-        subplot(2,1,2)
+        subplot(3,1,3)
         showFluctLevs(highest, lowest, tLevs);
         linFluct = lineSetter(gcf, 14);
         set(linFluct,'Visible','off')
         
-        subplot(2,1,2)
+        subplot(3,1,3)
         showHighLevs(highest, tLevs);
         linHi = lineSetter(gcf, 14);
         set(linHi,'Visible','off')
@@ -149,12 +164,18 @@ while(true)
     
     
     
-    subplot(2,1,1)
+    subplot(3,1,1)
     axis([w(end - startIndx,1), w(end - endIndx,1)+2,...
         min(w(end-endIndx:end-startIndx,4))*0.95, max(w(end-endIndx:end-startIndx,3))*1.05]);
     datetick('x',12, 'keeplimits');
+  
+%     subplot(3,1,2)
+%     axis([w(end - (startIndx+90),1), w(end - endIndx,1)+2,...
+%         min(w(end-endIndx:end-(startIndx+90),4))*0.95,...
+%         max(w(end-endIndx:end-(startIndx+90),3))*1.05]);
+%     datetick('x',12, 'keeplimits');
     
-    subplot(2,1,2)
+    subplot(3,1,3)
     axis([w(end - startIndx,1), w(end - (endIndx+4),1)+2,...
         min(w(end-(endIndx+4):end-startIndx,4))*0.95, max(w(end-(endIndx+4):end-startIndx,3))*1.05]);
     datetick('x',12, 'keeplimits');
--- lineSetter.m
+++ lineSetter.m
@@ -9,17 +9,3 @@ lineHandle = findobj(dataObjs(1:numOfLines), 'type', 'line');
 
 end
 
-
-
-
-
-
-% dataObjs = get(axesObjs(1), 'Children');
-% objTypes = get(dataObjs(1:numOfLines), 'Type');
-% lineHandle = findobj(dataObjs(1:numOfLines), 'type', 'line');
-% 
-% 
-% 
-% dataObjs = findobj(axesObjs, 'type', 'axes');
-% choosenDataObj = dataObjs(1);
-% get(axesObjs, 'Type');
