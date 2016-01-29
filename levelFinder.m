c7ec219152d048193ee5be41fbd0a90d4d55cf3d
SteveErle <therevolution777@yahoo.com>
2016-01-28 22:20:53 -0500

    levelFinder updates

--- levelFinder.m
+++ levelFinder.m
@@ -1,22 +1,82 @@
 clear; close all; clc;
 
-stock = 'PSEC'
+stock = 'AXL'
 
 minutely = IntraDayStockData(stock,'NYSE','60','1d');
 c = yahoo;
-m = fetch(c,stock,now, now-17000, 'm');
-d = fetch(c,stock,now, now-17000, 'd');
+m = fetch(c,stock,now, now-500, 'm');
+d = fetch(c,stock,now, now-500, 'd');
 close(c)
+ 
+
+for begin = 2:1:100
+    
+    set(gcf, 'Position', [9,5,1423,800])
+    hi = d(:,3); lo = d(:,4); cl = d(:,5); op = d(:,2); da = d(:,1);
+    highlow(hi, lo, op, cl, 'black', da);
+    % axis([da(100), da(1)+5,...
+    %     min(lo(1:100))*0.95, max(hi(1:100))*1.05])
+    title(strcat(stock,' Daily'))
+    datetick('x',12, 'keeplimits');
+    hold on
+    
+    
+    founder = [];
+    
+    for i = 10:50
+        
+        res = max(hi(begin:begin+i));
+        sup = min(lo(begin:begin+i));
+        
+        founder = [founder; begin + i, (res-sup)/sup];
+        
+    end
+    
+    
+    uniqueNY = unique(founder(:,2));
+    foundX = [];
+    for i = 1:length(uniqueNY)
+        
+        specific = find(uniqueNY(i) == founder(:,2));
+        
+        foundX = [foundX; uniqueNY(i), max(founder(specific,1))];
+        
+      end
+    
+  
+    
+    for i = 1: size(foundX,1)
+        
+        indxStart   = begin;
+        indxEnd     = foundX(i,2);
+        
+        foundDates  = da(indxStart:indxEnd);
+        foundRes    = max(hi(indxStart:indxEnd));
+        foundSup    = min(lo(indxStart:indxEnd));
+        
+        plot(foundDates, ones(length(foundDates),1)*foundRes, 'b')
+        plot(foundDates, ones(length(foundDates),1)*foundSup, 'b')
+        
+%         if hi(begin-1)- 
+        plot(da(begin-1), hi(begin-1), 'mo')
+        
+        
+    end
+    
+    pause
+    close all
+    
+end
+
+
+return
 
 
 
-hi = d(:,3); lo = d(:,4); cl = d(:,5); op = d(:,2); da = d(:,1);
-highlow(hi, lo, op, cl, 'black', da);
-% axis([da(100), da(1)+5,...
-%     min(lo(1:100))*0.95, max(hi(1:100))*1.05])
-title(strcat(stock,' Daily'))
-datetick('x',12, 'keeplimits');
-hold on
+
+
+
+
 
 foundMeSalson = [];
 for j = 50:1000
@@ -36,30 +96,22 @@ for j = 50:1000
     j
 end
 
-uniqueNY = unique(foundMeSalson(:,1));
-foundX = [];
-for i = 1:length(uniqueNY)
 
-specific = find(uniqueNY(i) == foundMeSalson(:,1));
-
-foundX = [foundX; uniqueNY(i), max(foundMeSalson(specific,2))];
-
-end 
 
 
 for i = 1:length(foundX)
-
-indxStart   = foundX(i,1)
-indxEnd     = foundX(i,1) + foundX(i,2)
-
-foundDates  = da(indxStart:indxEnd);
-foundRes    = max(hi(indxStart:indxEnd));
-foundSup    = min(lo(indxStart:indxEnd));
-
-plot(foundDates, ones(length(foundDates),1)*foundRes, 'r')
-plot(foundDates, ones(length(foundDates),1)*foundSup, 'r')
-pause
-end 
+    
+    indxStart   = foundX(i,1)
+    indxEnd     = foundX(i,1) + foundX(i,2)
+    
+    foundDates  = da(indxStart:indxEnd);
+    foundRes    = max(hi(indxStart:indxEnd));
+    foundSup    = min(lo(indxStart:indxEnd));
+    
+    plot(foundDates, ones(length(foundDates),1)*foundRes, 'r')
+    plot(foundDates, ones(length(foundDates),1)*foundSup, 'r')
+    pause
+end
 
 % numFound = [];
 % for i = 1:length(hi)
