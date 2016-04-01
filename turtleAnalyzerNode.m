30d89a0f048dbe2f9933b0a6359da0836a8ba777
SteveErle <therevolution777@yahoo.com>
2016-04-01 19:41:05 -0400

    trying new percent Scaling

--- turtleAnalyzerNode.m
+++ turtleAnalyzerNode.m
@@ -3,10 +3,10 @@
 clear all; clc; close all;
 
 
-beta = 1.29;
+beta = 1.0;
 
-past = '1/1/16';
-simulateTo = now;
+past = '1/1/15';
+simulateTo = now-100;
 
 stock = 'TSLA';
 
@@ -23,90 +23,79 @@ close(c);
 
 tf = TurtleFun;
 
-
-
 [hi, lo, cl, op, da] = tf.returnOHLCDarray(dAll);
-
 
 % Idx = cos(2*pi*1/33*(1:100))' + 10;
 % Scb = cos(2*pi*1/17*(1:100) + pi/2)' + 10;
 
-Idx = dAvg(:,5:5);
-Scb = dAll(:,5:5);
+Idx = dAvg(:,2:5);
+Scb = dAll(:,2:5);
 
 per = [];
 
-
 for i = 1 : size(Idx,1) - 1
-    per = [per; beta* ((Idx(i,:) - Idx(i+1,:)) ./ Idx(i+1,:))];
-end 
+    per = [per; beta * ((Idx(i,:) - Idx(i+1,:)) ./ Idx(i+1,:))];
+end
 
 Sio = Scb(1,:);
 
 for i = 1 : size(Idx,1) - 1
   Sio(i+1,:) = Sio(i,:) ./ (1+per(i,:));
-end 
-
+end
 
 Sro = Scb(1) + (Scb - Sio);
 
-ScbF = diff(getFiltered(Scb,0.1,'low'));
-SioF = diff(getFiltered(Sio,0.1,'low'));
-SroF = diff(getFiltered(Sro,0.1,'low'));
+
+wSize = 20;
+ScbS = flipud(tsmovavg(flipud(Scb),'s',wSize,1));
+SioS = flipud(tsmovavg(flipud(Sio),'s',wSize,1));
+SroS = flipud(tsmovavg(flipud(Sro),'s',wSize,1));
 
 
-wSize = 10;
-ScbS = (tsmovavg(flipud(Scb),'s',wSize,1));
-SioS = (tsmovavg(flipud(Sio),'s',wSize,1));
-SroS = (tsmovavg(flipud(Sro),'s',wSize,1));
+% ScbS(end:end-wSize) = ScbS(end-wSize);
 
-
-ScbS(end:end-wSize) = ScbS(end-wSize);
-
-% subplot(2,1,1)
+subplot(2,1,1)
 hold on
-% [hi, lo, cl, op, da] = tf.returnOHLCDarray(dAll);
-% highlow(hi, lo, op, cl, 'red', da);
-plot(da,Sio)
-plot(da,Scb,'r')
-plot(da,Sro, 'g')
+[hi, lo, cl, op, da] = tf.returnOHLCDarray(dAvg);
+highlow(hi, lo, op, cl, 'black', da);
+% plot(da,Sio)
+
+% plot(da,Sro, 'g')
 
 
 % subplot(2,1,2)
-hold on
-plot(da(1:end),SioS)
-plot(da(1:end),ScbS,'r')
-plot(da(1:end),SroS, 'g')
-
-
-return 
-
-p = 0;
-while(true)
-    
-pause(0.2)
-    h = gcf;
-    axesObjs = get(h, 'Children');
-    axesObjs = findobj(axesObjs, 'type', 'axes');
-    dataTips = findall(axesObjs, 'Type', 'hggroup', 'HandleVisibility', 'off');
-    
-    if length(dataTips) > 0
-        
-        cursor = datacursormode(gcf);
-        values = cursor.getCursorInfo;
-        
-        tData = values.Position;
-        if ishandle(p) & p(1) ~= 0
-            delete(p)
-        end 
-        subplot(2,1,1)
-        p(1) = plot(tData(1), tData(2), 'bo');
-        subplot(2,1,2)
-        p(2) = plot(tData(1), tData(2), 'bo');
-    end
-    delete(dataTips);
-    
-end
+% hold on
+% plot(da(1:end),SioS)
+% plot(da,ScbS,'r')
+% plot(da(1:end),SroS, 'g')
+% 
+% 
+% p = 0;
+% while(true)
+%     
+% pause(0.2)
+%     h = gcf;
+%     axesObjs = get(h, 'Children');
+%     axesObjs = findobj(axesObjs, 'type', 'axes');
+%     dataTips = findall(axesObjs, 'Type', 'hggroup', 'HandleVisibility', 'off');
+%     
+%     if length(dataTips) > 0
+%         
+%         cursor = datacursormode(gcf);
+%         values = cursor.getCursorInfo;
+%         
+%         tData = values.Position;
+%         if ishandle(p) & p(1) ~= 0
+%             delete(p)
+%         end 
+%         subplot(2,1,1)
+%         p(1) = plot(tData(1), tData(2), 'bo');
+%         subplot(2,1,2)
+%         p(2) = plot(tData(1), tData(2), 'bo');
+%     end
+%     delete(dataTips);
+%     
+% end
 
 
 
@@ -114,13 +103,13 @@ end
 % [hi, lo, cl, op, da] = tf.returnOHLCDarray([da,Idx]);
 % highlow(hi, lo, op, cl, 'blue', da);
 % 
-% subplot(2,1,2)
-% hold on
-% [hi, lo, cl, op, da] = tf.returnOHLCDarray([da,Sio]);
-% highlow(hi, lo, op, cl, 'blue', da);
-% [hi, lo, cl, op, da] = tf.returnOHLCDarray([da,Scb]);
-% highlow(hi, lo, op, cl, 'red', da);
+subplot(2,1,2)
+hold on
+[hi, lo, cl, op, da] = tf.returnOHLCDarray([da,Sio]);
+highlow(hi, lo, op, cl, 'blue', da);
+[hi, lo, cl, op, da] = tf.returnOHLCDarray([da,Scb]);
+highlow(hi, lo, op, cl, 'red', da);
 % [hi, lo, cl, op, da] = tf.returnOHLCDarray([da,Sro]);
-% highlow(hi, lo, op, cl, 'green', da);
+% highlow(hi, lo, op, cl, 'black', da);
 
 
